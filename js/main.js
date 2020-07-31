cc6b30f6590a9d694172939df68dbea280132115
vuhongson2615 <vuhongson2615@gmail.com>
2020-07-31 15:19:59 +0700

    add- EditGame and fix DeleteGame

--- js/main.js
+++ js/main.js
@@ -21,3 +21,27 @@ function deleteGame(id) {
     manager.deleteGame(id);
     display(manager.getHtml());
 }
+let countId = 0;
+function updateGame(id) {
+    let game = manager.getGameById(id);
+    let name = document.getElementById('edit-name').value = game.name;
+    let img = document.getElementById('edit-img').value = game.img;
+    let price = document.getElementById('edit-price').value = game.price;
+    let version = document.getElementById('edit-version').value = game.version;
+    let quality = document.getElementById('edit-quality').value = game.quality;
+    let sell = document.getElementById('edit-sell').value = game.sell;
+    let desc = document.getElementById('edit-desc').value = game.desc;
+    countId =  id;
+}
+function editGame() {
+    let name = document.getElementById('edit-name').value;
+    let img = document.getElementById('edit-img').value;
+    let price = document.getElementById('edit-price').value;
+    let version = document.getElementById('edit-version').value;
+    let quality = document.getElementById('edit-quality').value;
+    let sell = document.getElementById('edit-sell').value;
+    let desc = document.getElementById('edit-desc').value;
+    manager.games[countId].updateGame(name, img, price, version, quality, sell, desc)
+    display(manager.getHtml());
+    document.getElementById('form-edit').reset();
+}
